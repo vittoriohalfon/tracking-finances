dbdd2fe9843d5e67ff55c056e8674254d970fe50
Vittorio <vittoriohalfon01@gmail.com>
2023-12-27 19:00:55 +0200

    temporarily disabled authenticateToken middleware for testing

--- routes/analytics.js
+++ routes/analytics.js
@@ -6,5 +6,7 @@ const { authenticateToken } = require('../services/authService');
 router.get('/budget', authenticateToken, getBudget);
 router.get('/trends', authenticateToken, getTrends);
 router.get('/alerts', authenticateToken, getAlerts);
+// router.post('/setBudget', authenticateToken, setBudget);
+router.post('/setBudget', setBudget);
 
 module.exports = router;
